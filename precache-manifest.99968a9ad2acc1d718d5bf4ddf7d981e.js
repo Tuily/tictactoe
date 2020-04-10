b56a9ef8c683df94280fa5b5fb98f0e4cb5d1db7
tuily <brizolla.tuily@gmail.com>
2020-04-10 14:50:53 -0300

    Updates

---
+++ precache-manifest.99968a9ad2acc1d718d5bf4ddf7d981e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1185f5400ef89f58188fda18b78ff67",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "2adb9c522784b0f470c4",
+    "url": "/tictactoe/static/css/main.89f3eb3c.chunk.css"
+  },
+  {
+    "revision": "7b612c68b09766644c12",
+    "url": "/tictactoe/static/js/2.c5a18dfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.c5a18dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2adb9c522784b0f470c4",
+    "url": "/tictactoe/static/js/main.bc650c93.chunk.js"
+  },
+  {
+    "revision": "d6a76b3547955ded53da",
+    "url": "/tictactoe/static/js/runtime-main.986fe205.js"
+  }
+]);
